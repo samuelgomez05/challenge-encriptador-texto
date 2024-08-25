a1773795b1ee9feac8d6357c5a12d9de0bbda935
Samuel Gomez <gomezapari20@gmail.com>
2024-08-24 21:00:49 -0500

    add logic for add class of error and animation when the user error in textarea and clean when is correct

--- js/app.js
+++ js/app.js
@@ -25,11 +25,14 @@ function logicaPrincipal(opcion) {
     let textoEncriptado = textarea.value;
 
     if (textoEncriptado === "") {
-        alert("El campo de texto es requerido");
+        asignarClaseError();
+        asignarTextoElemento(".process__text", "El campo de texto es requerido");
     } else if (contieneLetraEspecial === true) {
-        alert("Ingrese solo letras minúsculas y sin acentos");
+        asignarClaseError();
+        asignarTextoElemento(".process__text", "Ingrese solo letras minúsculas y sin acentos");
     } else if (contieneCaracterEspecial === true) {
-        alert("No se permiten caracteres especiales");
+        asignarClaseError();
+        asignarTextoElemento(".process__text", "No se permiten caracteres especiales");
     } else {
         // Recorremos todas las llaves de encriptacion
         for (let i = 0; i < llavesEncriptacion.length; i++) {
@@ -51,6 +54,10 @@ function logicaPrincipal(opcion) {
 
         // Asignamos el texto encriptado al parrafo del div view
         asignarTextoElemento(".view__paragraph", textoEncriptado);
+
+        // Removemos la clase de error y asignamos el texto de informacion correcta
+        removerClase(".process__info", "process__info--resalt");
+        asignarTextoElemento(".process__text", "Solo letras minúsculas y sin acentos");
     }
 }
 
@@ -63,15 +70,35 @@ function desencriptar() {
 }
 
 function asignarTextoElemento(selector, texto) {
-    let elemento = document.querySelector(selector);
+    const elemento = document.querySelector(selector);
 
     elemento.innerHTML = texto;
 }
 
 function asignarClaseElemento(selector, clase) {
-    let elemento = document.querySelector(selector);
+    const elemento = document.querySelector(selector);
 
     elemento.classList.add(clase);
+}
+
+function asignarClaseElementoConTiempo(selector, clase, tiempo) {
+    const elemento = document.querySelector(selector);
+    elemento.classList.add(clase);
+
+    setTimeout(() => {
+        elemento.classList.remove(clase);
+    }, tiempo);
+}
+
+function asignarClaseError() {
+    asignarClaseElemento(".process__info", "process__info--resalt");
+    asignarClaseElementoConTiempo(".process__info", "process__info--animation", 800);
+}
+
+function removerClase(selector, clase) {
+    const elemento = document.querySelector(selector);
+
+    elemento.classList.remove(clase);
 }
 
 function limpiarTextarea() {
